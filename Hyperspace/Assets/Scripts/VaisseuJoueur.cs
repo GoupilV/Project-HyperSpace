ab083e40b613e8c54cb3654fd8fcf4f8934786e4
baptistecrp <baptiste.crepon@etu.univ-smb.fr>
2024-06-12 14:52:16 +0200

    particule code mieux encore

--- Hyperspace/Assets/Scripts/VaisseuJoueur.cs
+++ Hyperspace/Assets/Scripts/VaisseuJoueur.cs
@@ -39,13 +39,6 @@ public class VaisseuJoueur : MonoBehaviour
         {
             exhaustParticleSystems = GetComponentsInChildren<ParticleSystem>();
         }
-
-        // Initialiser les rotations initiales
-        initialRotations = new Quaternion[exhaustParticleSystems.Length];
-        for (int i = 0; i < exhaustParticleSystems.Length; i++)
-        {
-            initialRotations[i] = exhaustParticleSystems[i].transform.localRotation;
-        }
     }
 
     // Update is called once per frame
@@ -61,11 +54,7 @@ public class VaisseuJoueur : MonoBehaviour
         // Activer/désactiver les particules en fonction du mouvement vertical
         if (mvVertical > 0)
         {
-            ActivateParticleSystems(exhaustParticleSystems, Vector3.forward);
-        }
-        else if (mvVertical < 0)
-        {
-            ActivateParticleSystems(exhaustParticleSystems, Vector3.back);
+            ActivateParticleSystems(exhaustParticleSystems);
         }
         else
         {
@@ -86,24 +75,13 @@ public class VaisseuJoueur : MonoBehaviour
         vaisseauRB.AddTorque(vaisseauRB.transform.forward * multVitesseAngleRoll * inputRoll, ForceMode.VelocityChange);
     }
 
-    void ActivateParticleSystems(ParticleSystem[] particleSystems, Vector3 direction)
+    void ActivateParticleSystems(ParticleSystem[] particleSystems)
     {
-        for (int i = 0; i < particleSystems.Length; i++)
+        foreach (var ps in particleSystems)
         {
-            var ps = particleSystems[i];
             if (!ps.isPlaying)
             {
                 ps.Play();
-            }
-
-            // Ajuster l'orientation des particules
-            if (direction == Vector3.forward)
-            {
-                ps.transform.localRotation = initialRotations[i];
-            }
-            else if (direction == Vector3.back)
-            {
-                ps.transform.localRotation = Quaternion.Euler(initialRotations[i].eulerAngles + new Vector3(0, 180, 0));
             }
         }
     }

019a50df8b7082f6a5be3bb3d90d2880bf21fb0f
Olppeami <mailys.demol-park@etu.univ-smb.fr>
2024-06-12 10:57:33 +0200

    Update Interface.cs

--- Hyperspace/Assets/Interface.cs
+++ Hyperspace/Assets/Interface.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interface : MonoBehaviour
 {
     List<string> optionNiveau = new List<string> {"Facile", "Intermédiaire", "Difficile"};
     Dropdown choixNiveau;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        // Récupérer le composant du DropDown choixNiveau
         choixNiveau = GetComponent<Dropdown>;
+
+        // Effacer et mettre les nouvelles options de niveau dans le Dropdown
         choixNiveau.ClearOptions();
         choixNiveau.AddOptions(optionNiveau);
+
+        //Ajouter un listener pour le changement de sélection
+        choixNiveau.onValueChanged.AddListener(delegate{SelctionNiveau(choixNiveau);});
+    }
+
+    void SelectionNiveau(DropDown niveauChoisi)
+    {
+        int index = niveauChoisi.value;
+        string niveau = optionNiveau[index];
+
+        if (niveau == "Facile")
+        {
+            Debug.Log("Niveau de difficulté sélectionné: Facile");
+
+
+        }
+        else if (niveau == "Intermédiaire")
+        {
+            Debug.Log("Niveau de difficulté sélectionné: Intermédiaire");
+        }
+        else if (niveau == "Difficile")
+        {
+            Debug.Log("Niveau de difficulté sélectionné: Difficile");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
+
+
 }
